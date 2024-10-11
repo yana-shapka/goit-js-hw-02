7c342607b85a2b60124fad7f79f92a6c06ba8c88
yanashapka <yana.shapka.test@gmail.com>
2024-10-11 18:43:08 +0300

    beautified task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,9 @@
 function formatMessage(message, maxLength) {
-  if (message.length === maxLength || message.length < maxLength) {
-    return message;
-  } else {
+  if (!(message.length === maxLength || message.length < maxLength)) {
     return `${message.slice(0, maxLength)}...`;
   }
+
+  return message;
 }
 
 console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
