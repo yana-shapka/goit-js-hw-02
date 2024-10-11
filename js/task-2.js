e5e5d47aa08ffe1e64d6cfb1c186ce91395d84e2
yanashapka <yana.shapka.test@gmail.com>
2024-10-11 18:39:34 +0300

    adjusted task 2

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@ function formatMessage(message, maxLength) {
   if (message.length === maxLength || message.length < maxLength) {
     return message;
   } else {
-    return message.slice(0, maxLength) + '...';
+    return `${message.slice(0, maxLength)}...`;
   }
 }
 
